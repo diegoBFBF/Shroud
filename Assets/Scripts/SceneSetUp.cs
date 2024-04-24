49f2599223aa1d2676cb1a84b2476911743cc14b
Kyle Hentschel <kylehntschel@gmail.com>
2024-04-24 18:37:41 -0400

    mixed reality stuff

--- Assets/Scripts/SceneSetUp.cs
+++ Assets/Scripts/SceneSetUp.cs
@@ -1,3 +1,4 @@
+using Meta.XR.MRUtilityKit;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using UnityEngine;
 public class SceneSetUp : MonoBehaviour
 {
     [SerializeField]
-    OVRSceneManager sceneManager;
+    MRUK mruk;
 
     [SerializeField]
     NoteTaker noteTaker;
@@ -16,61 +17,16 @@ public class SceneSetUp : MonoBehaviour
 
     private void Awake()
     {
-        sceneManager.SceneModelLoadedSuccessfully += OnSceneLoaded;
+        mruk.SceneLoadedEvent.AddListener(OnSceneLoaded);
     }
 
-    private async void OnSceneLoaded()
+    private void OnSceneLoaded()
     {
         Debug.Log("SceneSetUp - OnSceneLoaded");
-        //OVRSceneRoom room = FindObjectOfType<OVRSceneRoom>();
+        var room = mruk.GetCurrentRoom();
+        var anchor = room.Anchors.Find(t => t.AnchorLabels.Contains(OVRSceneManager.Classification.Table));
+        Vector3 newPosition = anchor.transform.position;
 
-        //Set Note Pad Position
-        var anchors = new List<OVRAnchor>();
-        await OVRAnchor.FetchAnchorsAsync<OVRRoomLayout>(anchors);
-
-        // no rooms - call Space Setup or check Scene permission
-        if (anchors.Count == 0)
-            return;
-
-        // access anchor data by retrieving the components
-        var room = anchors.First();
-
-        // access the ceiling, floor and walls with the OVRRoomLayout component
-        var roomLayout = room.GetComponent<OVRRoomLayout>();
-        if (roomLayout.TryGetRoomLayout(out Guid ceiling, out Guid floor, out Guid[] walls))
-        {
-            // use these guids to fetch the OVRAnchor object directly
-            await OVRAnchor.FetchAnchorsAsync(walls, anchors);
-        }
-
-        // access the list of children with the OVRAnchorContainer component
-        if (!room.TryGetComponent(out OVRAnchorContainer container))
-            return;
-
-        // use the component helper function to access all child anchors
-        await container.FetchChildrenAsync(anchors);
-
-        foreach (var anchor in anchors)
-        {
-            // check that this anchor is the table
-            if (!anchor.TryGetComponent(out OVRSemanticLabels labels) ||
-                !labels.Labels.Contains(OVRSceneManager.Classification.Table))
-            {
-                continue;
-            }
-
-            Debug.Log("SceneSetUp - Found Table" + anchor.Uuid);
-
-            // get the table dimensions
-            anchor.TryGetComponent(out OVRBounded3D bounded3D);
-            var size = bounded3D.BoundingBox.size;
-
-            Vector3 newPosition = bounded3D.BoundingBox.center;
-
-            //newPosition += Vector3.up * size.y;
-
-            noteTaker.SetPosition(newPosition);
-            break;
-        }
+        noteTaker.SetPosition(newPosition);
     }
 }
