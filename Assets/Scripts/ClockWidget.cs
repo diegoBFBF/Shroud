a48241938be91e7a6085ddf74c50849259c244a7
Kyle Hentschel <kylehntschel@gmail.com>
2024-04-25 11:31:45 -0400

    before fix merge from main

---
+++ Assets/Scripts/ClockWidget.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockWidget : FocusWidget
+{
+    public override void HandleonFadeComplete()
+    {
+        
+    }
+}
